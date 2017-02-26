edd487c901ca94534b0b9222bb0fe1d907bd8316
Apichaya Tiwcharoensakul <apichaya.ti@ku.th>
2017-02-26 21:42:59 +0700

    Update Main

--- src/coinpurse/Main.java
+++ src/coinpurse/Main.java
@@ -1,5 +1,6 @@
 package coinpurse;
 
+import java.util.ResourceBundle;
 import java.util.Scanner;
 
 /**
@@ -21,12 +22,25 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 
-		MoneyFactory.setMoneyFactory(new MalayMoneyFactory());
+		ResourceBundle bundle = ResourceBundle.getBundle("purse");
+		String factoryclass = bundle.getString("moneyfactory");
+		MoneyFactory factory = null;
+		try {
+			factory = (MoneyFactory) Class.forName(factoryclass).newInstance();
+		} catch (ClassCastException cce) {
+			System.out.println(factoryclass + " is not type MoneyFactor");
+		} catch (Exception ex) {
+			System.out
+					.println("Error creating MoneyFactory " + ex.getMessage());
+		}
+		if (factory == null)
+			System.exit(1);
+		else
+			MoneyFactory.setMoneyFactory(factory);
 
 		Purse purse = new Purse(CAPASITY);
 		ConsoleDialog ui = new ConsoleDialog(purse);
 		ui.run();
-
 
 	}
 
